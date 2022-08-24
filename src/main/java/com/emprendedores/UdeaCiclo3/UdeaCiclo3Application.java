275369fb3affe476f40dbfab5c253ef538613323
Alejandro Baquero <diegobaquero991@gmail.com>
2022-08-24 11:30:15 -0500

    Prueba de Conexion

--- src/main/java/com/emprendedores/UdeaCiclo3/UdeaCiclo3Application.java
+++ src/main/java/com/emprendedores/UdeaCiclo3/UdeaCiclo3Application.java
@@ -1,13 +1,25 @@
 package com.emprendedores.UdeaCiclo3;
-
+// Importe de librerias
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude ={SecurityAutoConfiguration.class})
 public class UdeaCiclo3Application {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola Cliclo 3...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdeaCiclo3Application.class, args);
+//		System.out.println("Hola, mundo");
+
 	}
+
 
 }
