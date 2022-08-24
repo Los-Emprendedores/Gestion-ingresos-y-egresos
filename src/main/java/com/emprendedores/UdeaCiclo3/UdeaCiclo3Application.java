274221b233835ff9f3b037f1546076191aabe85c
Alejandro Baquero <diegobaquero991@gmail.com>
2022-08-24 09:41:33 -0500

    Complementos sprint

---
+++ src/main/java/com/emprendedores/UdeaCiclo3/UdeaCiclo3Application.java
@@ -0,0 +1,13 @@
+package com.emprendedores.UdeaCiclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdeaCiclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdeaCiclo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/emprendedores/UdeaCiclo3/UdeaCiclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emprendedores.UdeaCiclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdeaCiclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
