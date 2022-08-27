e8723a85f321c74eea1794e966842e1d128fcb3d
Alejandro Baquero <diegobaquero991@gmail.com>
2022-08-27 16:39:21 -0500

    Se crearon, servicios de MovimientoDinero, finalizado

--- src/main/java/com/emprendedores/UdeaCiclo3/service/MovimientoDineroService.java
+++ src/main/java/com/emprendedores/UdeaCiclo3/service/MovimientoDineroService.java
@@ -5,7 +5,8 @@ import com.emprendedores.UdeaCiclo3.repo.MovimientoDineroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service //Anotacion que indica que es un servicio
 public class MovimientoDineroService {
@@ -19,5 +20,19 @@ public class MovimientoDineroService {
         return movimientoDineroList;
     }
 
+    //Metodo que trae un objeto de tipo movimientoDinero cuando se tiene el id
+    public MovimientoDinero getMovimientoDineroById(Integer id){
+        return movimientoDineroRepository.findById(id).get();
+    }
+
+    //Metodo para guardar o actualizar objetos de tipo movimientoDinero
+
+    public boolean saveUpdateMovimientoDinero(MovimientoDinero movimientoDinero){
+        MovimientoDinero mov=movimientoDineroRepository.save(movimientoDinero);
+        if(movimientoDineroRepository.findById(mov.getId())!=null){
+            return true;
+        }
+        return false;
+    }
 
 }
