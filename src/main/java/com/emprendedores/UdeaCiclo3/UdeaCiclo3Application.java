5242e148b0fc7d6a33b348ab0ba3a1b81b83d520
Alejandro Baquero <diegobaquero991@gmail.com>
2022-08-29 17:01:46 -0500

    Se agrego, listar empresas y agregar empresas, desde la aplicacion web.

--- src/main/java/com/emprendedores/UdeaCiclo3/UdeaCiclo3Application.java
+++ src/main/java/com/emprendedores/UdeaCiclo3/UdeaCiclo3Application.java
@@ -1,14 +1,9 @@
 package com.emprendedores.UdeaCiclo3;
 // Importe de librerias
 
-import com.emprendedores.UdeaCiclo3.Entidades.Empleado;
-import com.emprendedores.UdeaCiclo3.Entidades.Empresa; // Impotar clase empresa
-import com.emprendedores.UdeaCiclo3.Entidades.MovimientoDinero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication(exclude ={SecurityAutoConfiguration.class})
@@ -19,13 +14,10 @@ public class UdeaCiclo3Application {
 	public String hello(){
 		return "Hola Cliclo 3...";
 	}
-
 	@GetMapping("/Empresa")// GetMapping Prueba Empresa
 	public String Empresa(){Empresa empr = new Empresa("STARK INDUSTRIES", "Calle 94b # 57a - 26", "3206217113", "900321745-1");
-
 		// Editar atributos
 		empr.setNombre("<b>STARK INDUSTRIES LTDA</b>");
-
 		// Retorno en pagina
 		return "<br>NOMBRE: " + empr.getNombre() + "</br>" +
 				"<br>DIRECCION: " +  empr.getDireccion() + "</br>" +
@@ -34,16 +26,14 @@ public class UdeaCiclo3Application {
 	}
 	@GetMapping("/Empleado")// GetMapping Prueba Empresa
 	public String Empleado(){Empleado empl = new Empleado(/*"Nombre", email, "empresa", "rol");
-
 		return "";
 	}
-
 	@GetMapping("/Movimientos")//GetMapping prueba movimientos
 	public String Movimientos(){MovimientoDinero mov = new MovimientoDinero(/*"monto", "concepto", usuario);
 		return "";
 	}*/
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(UdeaCiclo3Application.class, args);
 //		System.out.println("Hola, mundo");
 
