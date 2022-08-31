f81cc5d2832ad13a3457b72c8814a27c54570aef
Alejandro Baquero <diegobaquero991@gmail.com>
2022-08-31 09:46:48 -0500

    Crud basico de la entidad empresa esta terminado

--- src/main/java/com/emprendedores/UdeaCiclo3/controller/Controlador.java
+++ src/main/java/com/emprendedores/UdeaCiclo3/controller/Controlador.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,11 +26,49 @@ public class Controlador {
         model.addAttribute("emplist", listaEmpresas);
         return "verEmpresas"; // Llama a HTML
     }
-
+    // Controlador para crear empresa en la base de datos
     @GetMapping("/AgregarEmpresa")
     public String nuevaEmpresa(Model model){
         Empresa emp = new Empresa();
         model.addAttribute("emp", emp);
         return "agregarEmpresa";
+    }
+    // Controlador para guardar empresa en la base de datos
+    @PostMapping("/GuardarEmpresa")
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
+        if (empresaService.saveUpdateEmpresa(emp)==true){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/AgregarEmpresa";
+    }
+
+    // Controlador para editar empresa en la base de datos
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Integer id){
+        Empresa emp=empresaService.getEmpresaById(id);
+        // Se creo atributo para el modelo, que se llame igualmente emp que iráal html para llenar los campos
+        model.addAttribute("emp", emp);
+        return "editarEmpresa";
+    }
+
+    // Controlador para actualizar la empresa en la base de datos
+    @PostMapping("/ActualizarEmpresa")
+    public String actualizarEmpresa(@ModelAttribute("emp") Empresa emp){
+        if (empresaService.saveUpdateEmpresa(emp)==true){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/EditarEmpresa";
+    }
+
+    //Controlador para eliminar la empresa de la base
+    @GetMapping("/EliminarEmpresa/{id}")
+    public String eliminarEmpresa(@PathVariable Integer id){
+        try {
+            empresaService.deleteEmpresa(id);
+        }
+        catch (Exception e){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/VerEmpresas";
     }
 }
